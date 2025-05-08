6309cc27c92b48d85e880e7f4f3f7b905b13699f
regalos <regalos1561@gmail.com>
2025-05-09 04:05:11 +0700

    Tralalero Tralala

--- src/main/java/com/podnet/podnet/service/MessengerService.java
+++ src/main/java/com/podnet/podnet/service/MessengerService.java
@@ -6,12 +6,15 @@ import com.podnet.podnet.entity.Message;
 import com.podnet.podnet.entity.User;
 import com.podnet.podnet.repository.ChatRepository;
 import com.podnet.podnet.repository.MessageRepository;
+import com.podnet.podnet.repository.UserRepository;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
@@ -20,6 +23,23 @@ public class MessengerService {
     private final UserServiceImpl userServiceImpl;
     private final ChatRepository chatRepository;
     private final MessageRepository messageRepository;
+    private final UserRepository userRepository;
+
+    @Transactional
+    public Chat createGroupChat(String chatName, Set<Long> memberIds) {
+        User creator = userServiceImpl.getCurrentUser()
+                .orElseThrow(() -> new EntityNotFoundException("User not found"));
+
+        List<User> members = userRepository.findAllById(memberIds);
+        members.add(creator); // Добавляем создателя в участники
+
+        return chatRepository.save(
+                Chat.builder()
+                        .name(chatName)
+                        .users(members)
+                        .build()
+        );
+    }
 
     public MessageDto putMessage(Long chatId, String text) {
         Chat chat = chatRepository.findById(chatId)
@@ -42,7 +62,6 @@ public class MessengerService {
                 .chat(chat)
                 .build();
     }
-
     private MessageDto mapToDto(Message message) {
         return new MessageDto(
                 message.getId(),
